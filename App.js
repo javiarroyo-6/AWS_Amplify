7d5df2e2f61a0eea886b21e9260504d9165c70e9
Javier Arroyo <arroyoj3@uci.edu>
2020-02-28 13:25:16 -0800

    Initial Commit

--- App.js
+++ App.js
@@ -1,19 +1,24 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import Amplify from "aws-amplify";
+import awsconfig from "./aws-exports";
+import { withAuthenticator } from "aws-amplify-react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
+Amplify.configure(awsconfig);
+
+const App = () => (
+  <View style={styles.container}>
+    <Text> Hello World !</Text>
+  </View>
+);
+
+export default withAuthenticator(App, true);
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
